7a0fb5c4d99f0431b1cb22420af020a35d06a849
Robby Baur <rbaur@hs-ulm.de>
2023-11-08 20:22:38 +0100

    Version01 Verbindung mit DB funktioniert und tabellen werden erstllen

--- src/Entitys/Kunde.java
+++ src/Entitys/Kunde.java
@@ -4,9 +4,20 @@ import jakarta.persistence.*;
 @Entity
 @Table(name="Kunde")
 public class Kunde {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Basic(optional = false)
     @Column(name = "KundenNr")
     private int kdnr;
+
+    @Basic
+    @Column(name = "Vorname")
+    private String vorname;
+
+    @Basic
+    @Column(name = "Nachname")
+    private String nachname;
+
+
 }
